786d1807563ffcf463d730ab137192c51c5a0b80
Dell\Krzysiek <skrzychu7@gmail.com>
2017-02-08 14:03:06 +0100

    Adding ListView

--- XBreweryDbApp/XBreweryDbApp/XBreweryDbApp/Models/Brewery.cs
+++ XBreweryDbApp/XBreweryDbApp/XBreweryDbApp/Models/Brewery.cs
@@ -3,15 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
 namespace XBreweryDbApp
 {
-    public partial class MainPage : ContentPage
+   public class Brewery
     {
-        public MainPage()
-        {
-            InitializeComponent();
-        }
+        public string Name { get; set; }
     }
 }
---
+++ XBreweryDbApp/XBreweryDbApp/XBreweryDbApp/Views/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace XBreweryDbApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BreweryListView.ItemsSource = GetItems();
+        }
+
+        private ObservableCollection<Brewery> GetItems()
+        {
+            var breweries = new ObservableCollection<Brewery>();
+            for (var i = 1; i <= 20; i++)
+            {
+                breweries.Add(new Brewery {Name = "Brewery " + i});
+            }
+
+            return breweries;
+        }
+    }
+}
