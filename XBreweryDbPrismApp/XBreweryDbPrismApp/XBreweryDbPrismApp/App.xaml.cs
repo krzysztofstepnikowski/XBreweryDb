af4555db9ae6d72471b8a3e31bfd02d3d27b7c11
Dell\Krzysiek <skrzychu7@gmail.com>
2017-02-16 12:38:12 +0100

    Navigation between pages

--- XBreweryDbPrismApp/XBreweryDbPrismApp/XBreweryDbPrismApp/App.xaml.cs
+++ XBreweryDbPrismApp/XBreweryDbPrismApp/XBreweryDbPrismApp/App.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic;
+using Autofac.Core;
 using Microsoft.Practices.Unity;
 using Prism.Unity;
 using Xamarin.Forms;
+using XBreweryDbPrismApp.Features.Details;
+using XBreweryDbPrismApp.Features.Features.BreweryDescription;
 using XBreweryDbPrismApp.Features.Features.BreweryList;
 using XBreweryDbPrismApp.Features.Features.Favorite;
 using XBreweryDbPrismApp.Features.Main;
@@ -26,14 +29,16 @@ namespace XBreweryDbPrismApp
         protected override void RegisterTypes()
         {
             Container.RegisterTypeForNavigation<NavigationPage>();
-            Container.RegisterTypeForNavigation<MainPage, MainPageViewModel>();
-            Container.RegisterTypeForNavigation<DetailPage>();
+            Container.RegisterTypeForNavigation<MainPage>();
+           Container.RegisterTypeForNavigation<DetailPage>();
 
 
             var favoriteBrewerage = new HashSet<string>();
             var favoriteManager = new FavoriteBreweryManager(favoriteBrewerage);
             Container.RegisterInstance<IMainPageFeatures>(new MainPageFeature(favoriteManager,
                 new BreweryListProvider(favoriteBrewerage)));
+
+            Container.RegisterInstance<IDetailPageFeatures>(new DetailPageFeature(new BreweryDescriptionProvider(), favoriteManager));
         }
     }
 }
--- XBreweryDbPrismApp/XBreweryDbPrismApp/XBreweryDbPrismApp/ViewModels/DetailPageViewModel.cs
+++ XBreweryDbPrismApp/XBreweryDbPrismApp/XBreweryDbPrismApp/ViewModels/DetailPageViewModel.cs
@@ -7,7 +7,7 @@ namespace XBreweryDbPrismApp.ViewModels
 {
     public class DetailPageViewModel : BindableBase, INavigationAware
     {
-        private readonly IDetailPageFeatures _detailPageFeatures;
+        private IDetailPageFeatures _detailPageFeatures;
 
         private string _description;
 
@@ -25,18 +25,22 @@ namespace XBreweryDbPrismApp.ViewModels
 
         public void OnNavigatedFrom(NavigationParameters parameters)
         {
-            var id = Convert.ToString(parameters["id"]);
-            Description = _detailPageFeatures.GetBreweryDescription(id);
+            
         }
 
         public void OnNavigatedTo(NavigationParameters parameters)
         {
            
+            var id = Convert.ToString(parameters["id"]);
+            Description = _detailPageFeatures.GetBreweryDescription(id);
+
         }
 
         public void OnNavigatingTo(NavigationParameters parameters)
         {
            
         }
+
+       
     }
 }
