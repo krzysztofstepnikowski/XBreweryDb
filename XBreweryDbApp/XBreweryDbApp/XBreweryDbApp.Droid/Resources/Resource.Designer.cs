b8ce4609d7a219eb5b0bd0091428703bb050e00c
Dell\Krzysiek <skrzychu7@gmail.com>
2017-02-21 12:05:21 +0100

    Resource.Designer

--- XBreweryDbApp/XBreweryDbApp/XBreweryDbApp.Droid/Resources/Resource.Designer.cs
+++ XBreweryDbApp/XBreweryDbApp/XBreweryDbApp.Droid/Resources/Resource.Designer.cs
@@ -26,8 +26,6 @@ namespace XBreweryDbApp.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::GalaSoft.MvvmLight.Resource.String.ApplicationName = global::XBreweryDbApp.Droid.Resource.String.ApplicationName;
-			global::GalaSoft.MvvmLight.Resource.String.Hello = global::XBreweryDbApp.Droid.Resource.String.Hello;
 			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::XBreweryDbApp.Droid.Resource.Attribute.actionBarSize;
 		}
 		
@@ -2947,12 +2945,6 @@ namespace XBreweryDbApp.Droid
 		
 		public partial class String
 		{
-			
-			// aapt resource value: 0x7f080027
-			public const int ApplicationName = 2131230759;
-			
-			// aapt resource value: 0x7f080026
-			public const int Hello = 2131230758;
 			
 			// aapt resource value: 0x7f08000f
 			public const int abc_action_bar_home_description = 2131230735;
